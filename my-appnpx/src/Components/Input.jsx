8482d6691754e3d1c425d2e7000ccec5ddb5ecd7
Mohamed EL HADDADI <77499801+Medhaddadi@users.noreply.github.com>
2023-11-25 21:09:34 +0000

    init the project

---
+++ my-appnpx/src/Components/Input.jsx
@@ -0,0 +1,10 @@
+export function Input(){
+    return (
+        <>
+        <label for="searchValue"> search</label>
+        <input type="text" id="searchValue" name="searchValue"/>
+        </>
+
+    );
+
+}
---
+++ my-appnpx/src/model/Product.js
@@ -0,0 +1,9 @@
+class Product{
+    constructor(category , price ,stocked,name ){
+        this.category=category;
+        this.price=price;
+        this.name=name;
+        this.stocked=stocked;
+    }
+
+}
